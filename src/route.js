12a0e02478a77f9678b313021f465e018b9354a6
xiongfei1121 <165855805+xiongfei1121@users.noreply.github.com>
2026-01-05 13:30:10 +0800

    Update route.js

--- src/route.js
+++ src/route.js
@@ -4,10 +4,11 @@ import bilibili_user_video from './lib/bilibili/user/video';
 import telegram_channel from './lib/telegram/channel';
 import weibo_user from './lib/weibo/user';
 import xiaohongshu_user from './lib/xiaohongshu/user';
+import handleAnkangGov from './lib/ankang/gov.js';
 
 const route = new Hono();
 
-let plugins = [bilibili_user_dynamic, bilibili_user_video, telegram_channel, weibo_user, xiaohongshu_user];
+let plugins = [bilibili_user_dynamic, bilibili_user_video, telegram_channel, weibo_user, xiaohongshu_user,handleAnkangGov];
 
 for (let plugin of plugins) {
 	plugin.setup(route);
